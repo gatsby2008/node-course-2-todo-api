3a065463eaaf1c0420baa86ba6eaa825b0c98589
Marcelo <marcesotero@yahoo.com>
2018-08-24 19:50:37 -0300

    Add GET /todos/:id

--- playground/mongoose-queries.js
+++ playground/mongoose-queries.js
@@ -2,29 +2,29 @@ const {mongoose} = require('./../server/db/mongoose')
 const {Todo} = require('./../server/models/todo')
 const {ObjectID} = require('mongodb')
 const {User} = require('./../server/models/user')
-// var id = '5b80799f806a15f74bae8241'
-// if (!ObjectID.isValid(id)) {
-//   console.log('ID not valid')
-// }
-//
-// Todo.find({
-//   _id: id
-// }).then((todos) => {
-//   console.log('Todos', todos)
-// })
-//
-// Todo.findOne({
-//   _id: id
-// }).then((todo) => {
-//   console.log('Todo', todo)
-// })
-//
-// Todo.findById(id).then((todo) => {
-//   if (!todo) {
-//     console.log('Id not found')
-//   }
-//   console.log('Todo By Id', todo)
-// }).catch((e) => console.log(e))
+var id = '5b80799f806a15f74bae8241'
+if (!ObjectID.isValid(id)) {
+  console.log('ID not valid')
+}
+
+Todo.find({
+  _id: id
+}).then((todos) => {
+  console.log('Todos', todos)
+})
+
+Todo.findOne({
+  _id: id
+}).then((todo) => {
+  console.log('Todo', todo)
+})
+
+Todo.findById(id).then((todo) => {
+  if (!todo) {
+    console.log('Id not found')
+  }
+  console.log('Todo By Id', todo)
+}).catch((e) => console.log(e))
 var userId = '5b608c8bddd0a11379e03ddf'
 User.findById(userId).then((user) => {
   if (!user) {
--- server/server.js
+++ server/server.js
@@ -4,6 +4,7 @@ var bodyParser = require('body-parser')
 var {mongoose} = require('./db/mongoose')
 var {Todo} = require('./models/todo')
 var {User} = require('./models/user')
+const {ObjectID} = require('mongodb')
 
 var app =  express()
 
@@ -27,6 +28,23 @@ app.get('/todos', (req, res) => {
   }, (e) => {
     res.status(400).send(e)
   })
+})
+
+app.get('/todos/:id', (req, res) => {
+  var id = req.params.id
+  if (!ObjectID.isValid(id)) {
+    console.log('ID not valid')
+    res.status(404).send()
+  } else {
+    Todo.findById(id).then((todo) => {
+      if (!todo) {
+        console.log('ID not found')
+        res.status(404).send()
+      }
+      console.log('ID was found')
+      res.send({todo})
+    }).catch(() => res.status(400).send())
+  }
 })
 
 app.listen(3000, () => {
